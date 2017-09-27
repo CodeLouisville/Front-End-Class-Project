ce5896febc4cdcf66f05a39250327ec10de58564
Don Hansen <dhansen@area71.com>
2017-09-27 15:59:25 -0400

    updates to base files

--- js/app.js
+++ js/app.js
@@ -1,6 +1,47 @@
+// Scroll to Top
+
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
+
+
+// Owl Carousel
+
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    center:true,
+    navText: [$('.owl-next'),$('.owl-prev')],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        center:true,
+        nav: false
+      },
+      600: {
+        items: 3,
+        center:true,
+        nav: false
+      },
+      2000: {
+        items: 3,
+        loop: true,
+        center:true
+      }
+    }
+  })
+})
+
+
 // wow.js
 
 var html=document.querySelector("html");html.className=html.className.replace(/\bno-js\b/,"js");var wow=new WOW({boxClass:"animate-block",animateClass:"active",offset:1,mobile:!0,live:!0});wow.init();
+
 
 // email validation
 
